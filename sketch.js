e30009c925d895df791d1a75af51118077ca7f37
Rajgardade <rajgardade1234@gmail.com>
2020-12-28 20:53:06 +0530

    added a new platform in class 26

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var box1, pig1;
-
+var platform
 function preload(){
 
 backgroundimage = loadImage("sprites/bg.png")
@@ -21,6 +21,7 @@ function setup(){
  
      
     ground = new Ground(600,height,1200,20)
+    platform = new Ground(150,305,300,170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -64,4 +65,5 @@ function draw(){
 
     bird.display();
 
+    platform.display();
 }
